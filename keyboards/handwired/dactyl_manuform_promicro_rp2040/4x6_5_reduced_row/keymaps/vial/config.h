655b7252d512adbd491473d6ea300f5df32d27ad
estauss1 <estauss23@gmail.com>
2025-01-18 21:44:51 -0500

    4x6_5_reduced_row files

---
+++ keyboards/handwired/dactyl_manuform_promicro_rp2040/4x6_5_reduced_row/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFD, 0x6D, 0x26, 0x45, 0x76, 0x89, 0x49, 0x1E}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
